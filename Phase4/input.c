4c44214c8a1d249074f097359077fb198e5aff0d
vpkaushik11 <83803726+vpkaushik11@users.noreply.github.com>
2023-04-28 01:05:05 +0530

    Phase 4 Added

---
+++ Phase4/input.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    int a;
+    int x=1;
+    int y=2;
+    int z=3;
+    while(a<z){
+        a++;
+        char c;
+        float hi;
+    }
+    return 1;
+}
