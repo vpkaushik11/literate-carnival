ad2b3647602fda5355839b1bbcf3fd4b722b148f
vpkaushik11 <83803726+vpkaushik11@users.noreply.github.com>
2023-04-29 09:54:57 +0530

    Phase 6

---
+++ Phase6/convertr.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+// Structure to store a quadruple
+struct Quadruple {
+    string opcode;
+    string arg1;
+    string arg2;
+    string result;
+
+    Quadruple(string op, string a1, string a2, string res) {
+        opcode = op;
+        arg1 = a1;
+        arg2 = a2;
+        result = res;
+    }
+};
+
+// Function to convert ICG to Quadruples
+vector<Quadruple> icg_to_quadruples(vector<vector<string> > icg) {
+    vector<Quadruple> quadruples;
+
+    // Map to keep track of temporary variables
+    map<string, int> temp_var_count;
+    for (int i = 0; i < icg.size(); i++) {
+        vector<string> code = icg[i];
+
+        // Check the operator and generate the appropriate quadruple
+        if (code[0] == "+") {
+            // Addition operation
+            string temp_var = "t" + to_string(temp_var_count["t"]);
+            temp_var_count["t"]++;
+            quadruples.push_back(Quadruple("+", code[1], code[2], temp_var));
+            quadruples.push_back(Quadruple("=", temp_var, "", code[3]));
+        } else if (code[0] == "-") {
+            // Subtraction operation
+            string temp_var = "t" + to_string(temp_var_count["t"]);
+            temp_var_count["t"]++;
+
+            quadruples.push_back(Quadruple("-", code[1], code[2], temp_var));
+            quadruples.push_back(Quadruple("=", temp_var, "", code[3]));
+        } else if (code[0] == "*") {
+            // Multiplication operation
+            string temp_var = "t" + to_string(temp_var_count["t"]);
+            temp_var_count["t"]++;
+
+            quadruples.push_back(Quadruple("*", code[1], code[2], temp_var));
+            quadruples.push_back(Quadruple("=", temp_var, "", code[3]));
+        } else if (code[0] == "/") {
+            // Division operation
+            string temp_var = "t" + to_string(temp_var_count["t"]);
+            temp_var_count["t"]++;
+
+            quadruples.push_back(Quadruple("/", code[1], code[2], temp_var));
+            quadruples.push_back(Quadruple("=", temp_var, "", code[3]));
+        } else if (code[0] == "=") {
+            // Assignment operation
+            quadruples.push_back(Quadruple("=", code[1], "", code[3]));
+        } else if (code[0] == "goto") {
+            // Goto statement
+            quadruples.push_back(Quadruple("GOTO", "", "", code[1]));
+        } else if (code[0] == "if") {
+            // If statement
+            if(code[1] == "NOT"){
+                
+            }else{
+                quadruples.push_back(Quadruple("IF", code[1], code[2], code[4]));
+            }
+            quadruples.push_back(Quadruple("IF", code[1], code[2], code[4]));
+        } else if (code.size() == 1) {
+            // Label statement
+            quadruples.push_back(Quadruple("label", "", "", code[0].substr(0, code[0].size() - 1)));
+        }
+    }
+
+    return quadruples;
+}
+
+int main() {
+    // Sample ICG
+    fstream inp;
+    inp.open("output.txt", ios::in);
+    vector<vector<string> > ICG;
+    vector<string> bla;
+    if(inp.is_open()){
+        string x;
+        while (getline(inp, x))
+        {
+            if(x != "\n"){
+                bla.push_back(x);
+            }
+        }
+    }
+    for(int i = 0; i < bla.size(); i++){
+        vector<string> vec;
+
+        istringstream iss(bla[i]);
+        string word;
+
+        while (iss >> word) {
+            vec.push_back(word);
+        }
+        ICG.push_back(vec);
+    }
+    vector<Quadruple> q = icg_to_quadruples(ICG);
+    for(auto it : q){
+        cout << it.opcode << it.arg1 << it.arg2 << it.result << endl;
+    }
+}
