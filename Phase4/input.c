ffa2f9dfe24fb018d8f683ce544425e78b97349d
AbdulRasheed <abdulmdali@gmail.com>
2023-04-29 09:09:58 +0530

    Added:Reference Code

--- Phase4/input.c
+++ Phase4/input.c
@@ -4,17 +4,8 @@
 int main() {
     int a = 20;
     int x=1;
-    int y=2;
-    int z=3;
     while(a<z){
         a++;
-        if(a > 0){
-            x--;
-        }
-        else{
-            y--;
-        }
     }
-    a = 0;
     return 1;
 }
